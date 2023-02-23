45b683bd75366c2501084a98e72cefcb4bbed567
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-02-23 13:46:41 -0800

    missing numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers from 0 - 9
+ *
+ * Return: the content of x
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (x == 2 && x == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
