25c407491544531c2a8ec5ae1b99e8994be01224
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-02-23 12:42:38 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Function to check if c is between 0 -9
+ * @c: Integer that checks the validity of the function
+ *
+ * Return: 1 if success otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c >= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
