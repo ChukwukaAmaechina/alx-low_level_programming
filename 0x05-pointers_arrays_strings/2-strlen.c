023442516c41ec8478a9e51c01b5835f408fe39c
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-02-27 14:14:28 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - functions to count string length
+ * @s: string
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s != '0/'; s++)
+	{
+		a++
+	}
+	return(a)
+}
