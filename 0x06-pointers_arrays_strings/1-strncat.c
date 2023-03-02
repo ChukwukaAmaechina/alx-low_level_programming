ad8eddcc26a14495bbf2b08b392eb893801980dc
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-03-02 12:53:37 -0800

    string concat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,22 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * _strncat - Concatenates two strings
-  * @dest: destination value
-  * @src: source value
-  * @n: limit of the concatenation
-  *
-  * Return: pointer to the resulting string dest
-  */
+ * _strncat - Concatenates two strings
+ * @dest: The string to be appended
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dlen = 0, i = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[dlen])
-	{
-		dlen++;
-	}
+	while (dest[index++])
+		dest_len++;
 
-	while (i < n && src[i])
-	{
-		dest[dlen] = src[i];
-		dlen++;
-		i++;
-	}
-
-	dest[dlen + n + 1] = '\0';
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
