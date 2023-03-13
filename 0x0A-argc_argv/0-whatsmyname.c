cdd824ae4835c6518f2ce58067c0ef95c0a0f014
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-03-13 13:03:28 -0700

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - function to print name
@@ -11,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
-	_putchar("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
