d94fe9cc7bde3eab3a5414ae7520663d7eafc745
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-03-13 12:58:30 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function to print name
+ * @argc: argument count
+ * @argv: argument passed
+ *
+ * Return: Always return zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	_putchar("%s\n", argv[0]);
+
+	return (0);
+}
