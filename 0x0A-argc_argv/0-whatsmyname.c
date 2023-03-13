3493d2468ce7e3a171743e536bb537ef8b87b4ad
ChukwukaAmaechina <realchukwuka@yahoo.com>
2023-03-13 13:07:17 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,8 @@
  * Return: Always return zero
  */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void) argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
